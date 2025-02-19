44074f5d9c29530af7a0bb8d484c1d766db758ce
poopraveen <106433185+poopraveen@users.noreply.github.com>
2025-02-19 16:49:19 +0800

    Update store.ts
    Signed-off-by: poopraveen <106433185+poopraveen@users.noreply.github.com>

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import dataReducer from "./slices/dataSlice";
-import profileReducer from './slices/profileSlice';
-import { uploadImage } from './slices/profileSlice';
+import profileReducer, { uploadImage } from './slices/profileSlice';
 export const store = configureStore({
   reducer: {
     data: dataReducer,
@@ -10,8 +9,7 @@ export const store = configureStore({
 middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(uploadImage.middleware),
 },
-}
-);
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
