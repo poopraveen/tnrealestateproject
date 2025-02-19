033ba8b4650ce72b080ee167e7487e1f4e88e89b
poopraveen <106433185+poopraveen@users.noreply.github.com>
2025-02-19 16:54:21 +0800

    Update store.ts
    Update store.ts
    
    Signed-off-by: poopraveen <106433185+poopraveen@users.noreply.github.com>

--- src/store/store.ts
+++ src/store/store.ts
@@ -8,7 +8,6 @@ export const store = configureStore({
   },
 middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(uploadImage.middleware),
-},
 });
 
 export type RootState = ReturnType<typeof store.getState>;
